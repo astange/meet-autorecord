5fd1f17c0360818a825fd92f060586a8157724de
astange <stange@google.com>
2020-08-22 16:20:18 -0400

    Update contentscript.js
    Adds logic to log whenever a meeting has been left.

--- contentscript.js
+++ contentscript.js
@@ -5,13 +5,17 @@ var inMeeting = false
 var observer = new MutationObserver(function(mutations) {
   var newInMeeting = document.querySelector('div[data-second-screen') != null
 
-  if (newInMeeting && !inMeeting) {
-    inMeeting = true
-    document.querySelectorAll('div[role="button"]').forEach(function(match) {
-        if (match.innerHTML.includes("Record")) {
-            match.click()
-        }
-    })
+  if (newInMeeting != inMeeting) {
+    inMeeting = newInMeeting
+    if (inMeeting) {
+        document.querySelectorAll('div[role="button"]').forEach(function(match) {
+            if (match.innerHTML.includes("Record")) {
+                match.click()
+            }
+        })
+    } else {
+        console.log('left meeting')
+    }
   }
 });
 observer.observe(document, { childList: true, attributes: true, subtree: true})
